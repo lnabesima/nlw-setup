f55425cd9242d77a477ae8fa422ad178261d9908
Lucas Nabesima <lucas.nabesima@gmail.com>
2023-01-22 13:17:04 -0300

    feat: install prisma orm

---
+++ server/src/server.ts
@@ -0,0 +1,15 @@
+import Fastify from 'fastify';
+
+const app = Fastify();
+
+app.get('/', () => {
+  return 'Hello World';
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Server running on port 3333');
+  });
